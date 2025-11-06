24df0a03cbc22070e3e7b10ab5998f761694ed4b
Alok <ry7069577@gmail.com>
2025-11-06 12:12:00 +0530

    updated code

--- merge_alternately.py
+++ merge_alternately.py
@@ -2,7 +2,6 @@
 Write a Python program to merge them alternately — one element from s1, then one from s2.
 If one string has extra elements, append them at the end separated by " " (a space).
 input:
-
 s1 = "a,b,c,d"
 s2 = "e,f,g,h,i,j"
 
