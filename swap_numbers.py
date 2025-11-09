495efb0ac92c656a48452fe60bc8154ca41e01f3
Alok <ry7069577@gmail.com>
2025-11-09 16:14:01 +0530

    swap_numbers program

---
+++ swap_numbers.py
@@ -0,0 +1,29 @@
+
+#Swap Numbers
+
+a = 5
+b = 7
+def swap_number1(a,b):
+    a = a+b
+    b = a-b
+    a = a-b
+    return a,b
+
+print(swap_number1(a,b))
+
+def swap_number2(a,b):
+    a,b = b,a
+    return a,b
+
+print(swap_number2(a,b))
+
+
+
+def swap_number3(a,b):
+    c = a
+    a = b
+    b = c
+
+    return a,b
+
+print(swap_number3(a,b))
