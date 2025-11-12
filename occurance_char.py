e73b51a24f710197363859235b6aee4b268ee000
Alok <ry7069577@gmail.com>
2025-11-12 07:14:31 +0530

    updated code

---
+++ occurance_char.py
@@ -0,0 +1,21 @@
+"""Write a Python program to count the occurrences of each character in a string.
+
+Input: "mississippi"
+Output: {'m': 1, 'i': 4, 's': 4, 'p': 2}
+
+"""
+
+def occurance_count(s):
+    count = {}
+    for i in s:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    return count
+
+s = "mississippi"
+print(occurance_count(s))
+
+
+output: {'m': 1, 'i': 4, 's': 4, 'p': 2}
