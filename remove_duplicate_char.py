fe81355b22d6b18f2de2eb39cb8508fe16546d1f
Alok <ry7069577@gmail.com>
2025-11-13 15:07:12 +0530

    remove duplicate from string

---
+++ remove_duplicate_char.py
@@ -0,0 +1,22 @@
+"""write a program to remove duplicate from string without using set or inbuit method
+
+input:"edfasjkbfkeaskfdnkbsafkjbdsfsadjfbsdjkfbkjasbfdkjbefb"
+output: "edfasjkbn"
+"""
+
+
+
+s = "edfasjkbfkeaskfdnkbsafkjbdsfsadjfbsdjkfbkjasbfdkjbefb"
+
+
+def remove_duplicate_str(s):
+    res = []
+
+    for i in s:
+        if i not in res:
+            res.append(i)
+
+    res = ''.join(res)
+    return res
+
+print(remove_duplicate_str(s))
