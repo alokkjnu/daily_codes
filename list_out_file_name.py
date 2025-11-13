66c8049623ee57c0dcd9b80faab1d9ac33fe68e0
Alok <ry7069577@gmail.com>
2025-11-13 14:44:10 +0530

    list out file names from dir

---
+++ list_out_file_name.py
@@ -0,0 +1,13 @@
+""" write a program to list out files from a directory"""
+
+
+import os
+
+path = "/home/shivalok/Desktop/Daily Code/daily_codes"
+
+def get_file_name(path):
+    file_names = os.listdir(path)
+    return file_names
+
+
+print(get_file_name(path))
