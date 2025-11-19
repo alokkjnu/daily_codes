6bb457ff2ced61c3326d691375039faec6ce12b1
Alok <ry7069577@gmail.com>
2025-11-19 13:34:33 +0530

    fastapi reverse string

---
+++ fastapi_reverse_string.py
@@ -0,0 +1,23 @@
+from fastapi import  FastAPI,Request
+import uvicorn
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Data(BaseModel):
+    name:str
+
+@app.post("/")
+def test(name:Data):
+    name = name.name
+    res = ""
+    for i in name:
+        res = i+res
+
+    return {"response":res}
+
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=8000)
