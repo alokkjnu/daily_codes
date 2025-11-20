d02853ed64d4e987d84d4a01459ffaa2a708b8a0
Alok <ry7069577@gmail.com>
2025-11-20 20:42:43 +0530

    Find max repeated number

---
+++ repeated_max_number.py
@@ -0,0 +1,14 @@
+#What is a value repeated maximum number of times?
+l = [9,4,2,1,9,7,8,3,5,9,1,9,6]
+
+def max_repeated_num(l):
+    d = {}
+    for i in l:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    res = max(d)
+    return res
+    
+print(max_repeated_num(l))
