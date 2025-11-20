5a7f9b3a799d6dea7ed70144a40d9cf3d9fb1594
Alok <ry7069577@gmail.com>
2025-11-20 18:30:50 +0530

    devidenumberintoparts.py

---
+++ devidenumberintoparts.py
@@ -0,0 +1,11 @@
+#5
+#3,2
+# Write a program to divide a number into two parts such that the first part is greater than the second part by 1.
+
+def test(s):
+    mid = s//2
+    first_value = mid
+    second_value = mid+1
+    return first_value,second_value
+
+print(test(6))
