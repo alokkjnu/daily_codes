e337c13a75835d359f0d23f36b4fb6252b5def71
Alok <ry7069577@gmail.com>
2025-11-20 20:39:41 +0530

    find_longest.py

---
+++ find_longest.py
@@ -0,0 +1,13 @@
+#Find longest word and index
+s = ['Happy', 'birthday', 'to', 'you']
+
+
+def find_longest(s):
+    res = ""
+    longest_len = 0
+    for i in range(len(s)):
+        if len(res) < len(s[i]):
+            res = s[i]
+            longest_len = i
+    
+    return res,longest_len
