789d19bbb9b376b5b528bf20f7b5cd8bb820765d
Alok <ry7069577@gmail.com>
2025-11-20 18:31:18 +0530

    sortdict_list.py

---
+++ sortdict_list.py
@@ -0,0 +1,15 @@
+# "Write a Python program to sort the following list of dictionaries by the key name:"
+
+users = [
+    {"name": "Alok", "age": 25},
+    {"name": "Rahul", "age": 30},
+    {"name": "Priya", "age": 28}
+]
+
+def sort_list(users):
+
+    users.sort(key = lambda x: x['name'])
+
+    return users
+
+print(sort_list(users))
