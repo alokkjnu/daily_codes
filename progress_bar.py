cb7406db7d0182bed19f2a60b0a5ad496e7692c4
Alok <ry7069577@gmail.com>
2025-11-22 22:29:13 +0530

    added progres bad

---
+++ progress_bar.py
@@ -0,0 +1,14 @@
+from alive_progress import alive_bar
+import time
+n = 500
+with alive_bar(n) as bar:
+    for i in range(n):
+        time.sleep(0.05)
+        bar()
+
+
+from tqdm import tqdm
+import time
+
+for i in tqdm(range(100)):
+    time.sleep(0.05)
