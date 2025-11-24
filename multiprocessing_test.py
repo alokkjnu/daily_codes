c24e674bc1d398af5084e81fd019233dec3b7e07
Alok <ry7069577@gmail.com>
2025-11-25 01:53:02 +0530

    multiprocessing

---
+++ multiprocessing_test.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import time
+
+def square(n):
+    print(n*n)
+
+def cube(n):
+    print(n*n*n)
+
+m1 = multiprocessing.Process(target=square, args=(5,))
+m2 = multiprocessing.Process(target=cube, args=(4,))
+
+m1.start()
+m2.start()
+
+time.sleep(10)
+
+m1.join()
+m2.join()
+g
