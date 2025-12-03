570e3ab87100b2c53cb05e5acc711fe1fa7cfbbf
Alok <ry7069577@gmail.com>
2025-12-03 12:04:00 +0530

    updated code

--- find_longest.py
+++ find_longest.py
@@ -4,14 +4,13 @@ Write a python program to find longest word along with index in a list.
 """
 
 
-
 def find_longest(s):
 
     res = ""
     longest_len = 0
 
     for i in range(len(s)):
-        
+
         if len(res) < len(s[i]):
             res = s[i]
             longest_len = i
