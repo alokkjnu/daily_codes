041b1646e891598744f511f0787bb901361b2289
Alok <ry7069577@gmail.com>
2025-12-09 18:31:32 +0530

    updated code

--- divideandmix.py
+++ divideandmix.py
@@ -11,7 +11,30 @@
 
 # Then mix both halves in such a way that the final output becomes:
 
-# odlllreWH
+# odlllreoHW
 
 
 # Write a Python program to do this.
+
+
+def divideandmix(inp):
+    inp_len = len(inp)
+    mid = inp_len//2
+    first_str = inp[:mid]
+    last_str = inp[mid:]
+    first_str = reverse_str(first_str)
+    last_str = reverse_str(last_str)
+    res= ""
+    for i in range(len(first_str)):
+        res = res+first_str[i]
+        res = res+last_str[i]
+
+    return res
+def reverse_str(inp):
+    res = ""
+    for i in inp:
+        res = i+res
+    return res
+
+
+print(divideandmix("HelloWorld"))
