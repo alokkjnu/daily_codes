9d3259fba23cd6a0eba317bfc48a353601799b43
Alok <ry7069577@gmail.com>
2025-12-09 18:39:08 +0530

    updated code

--- repeateachchar.py
+++ repeateachchar.py
@@ -1,4 +1,3 @@
 # Given a string where each letter is followed by a number (example: "a2b3c4"), write a Python program to repeat each letter according to its number.
-# For example:
 # Input:  a2b3c4
 # Output: aabbbcccc
