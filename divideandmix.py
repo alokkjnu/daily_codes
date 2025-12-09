94e041955da7851e228749b55fe0f95b357211f1
Alok <ry7069577@gmail.com>
2025-12-09 18:21:04 +0530

    updated code

---
+++ divideandmix.py
@@ -0,0 +1,17 @@
+# Given the word:
+
+# HelloWorld
+
+
+# Divide it into two equal halves:
+
+# Hello
+
+# World
+
+# Then mix both halves in such a way that the final output becomes:
+
+# odlllreWH
+
+
+# Write a Python program to do this.
