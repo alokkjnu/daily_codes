7d96e5ec32efa1279d2163e1381f4dc789bda01e
Alok <ry7069577@gmail.com>
2025-12-12 12:01:00 +0530

    fixed admin dashboard issue

---
+++ class_based_oops/get_flat_list.py
@@ -0,0 +1,38 @@
+"""
+Write a python program to get flat list from a nested list.
+input: _list = [1, 2,[3], 4, 5,[6, 7]]
+output: [1, 2, 3, 4, 5, 6, 7] 
+"""
+
+class GetFlat:
+
+    def __init__(self,inp):
+
+        self.inp = inp
+
+    def get_flat_list(self):
+
+        try:
+            res = []
+            for i in self.inp:
+                if type(i) == list:
+                    for j in i:
+                        res.append(j)
+
+                else:
+
+                    res.append(i)
+            return res
+
+        except Exception as e:
+            return (str(e))
+
+if __name__=="__main__":
+
+    inp = [1, 2,[3], 4, 5,[6, 7]]
+    cls_obj = GetFlat(inp)
+    res = cls_obj.get_flat_list()
+
+    print(res)
+
+
