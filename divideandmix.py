1778524f075d363f891d22c1555b38319572ada7
Alok <ry7069577@gmail.com>
2025-12-14 16:25:53 +0530

    updated code

--- divideandmix.py
+++ divideandmix.py
@@ -1,28 +1,52 @@
-# Given the word:  "HelloWorld"
-# Divide it into two equal halves:
-# Hello
-# World
-# Then mix both halves in such a way that the final output becomes:
-# odlllreoHW
+"""
+Given the word:  "HelloWorld"
+Divide it into two equal halves:
+Hello
+World
+Then mix both halves in such a way that the final output becomes:
+output : odlllreoHW
+"""
 
-def divideandmix(inp):
-    inp_len = len(inp)
-    mid = inp_len//2
-    first_str = inp[:mid]
-    last_str = inp[mid:]
-    first_str = reverse_str(first_str)
-    last_str = reverse_str(last_str)
-    res= ""
-    for i in range(len(first_str)):
-        res = res+first_str[i]
-        res = res+last_str[i]
-    return res
+class DivideMix:
 
-def reverse_str(inp):
-    res = ""
-    for i in inp:
-        res = i+res
-    return res
+    def __init__(self,inp):
 
+        self.inp = inp
+        self.len_inp = len(inp)
 
-print(divideandmix("HelloWorld"))
+    def divide_mix(self):
+
+        mid = self.len_inp //2
+
+        first_str = self.inp[:mid]
+        last_str = self.inp[mid:]
+
+        first_str = self.reverse_str(first_str)
+        last_str = self.reverse_str(last_str)
+
+        res = ""
+
+        for i in range(len(first_str)):
+
+            res = res + first_str[i]
+            res = res +last_str[i]
+
+        return res
+    
+    def reverse_str(self,inp):
+        res = ""
+
+        for i in inp:
+            res = i +res
+        
+        return res
+
+if __name__ == "__main__":
+
+    inp = "HelloWorld"
+
+    cls_obj = DivideMix(inp)
+
+    res= cls_obj.divide_mix()
+
+    print(res)
