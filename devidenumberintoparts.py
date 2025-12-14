d71eca03b342166d9375279556540aff29b0dda1
Alok <ry7069577@gmail.com>
2025-12-14 14:08:04 +0530

    updated code

--- devidenumberintoparts.py
+++ devidenumberintoparts.py
@@ -1,14 +1,29 @@
 """
-Write a program to divide a number into two parts such that the first part is greater than the second part by 1.
+Write a program to divide a number into two parts such that,
+the first part is greater than the second part by 1.
 input: 5
 output: 3,2
 """
 
-def test(s):
-    
-    mid = s//2
-    first_value = mid
-    second_value = mid+1
-    return first_value,second_value
 
-print(test(6))
+class DivideTwo:
+
+    def __init__(self,inp):
+
+        self.inp = inp
+
+    def divide_into_two(self):
+
+        mid = self.inp //2
+        left = mid
+        right = mid + 1
+
+        return left,right
+
+
+if __name__ == "__main__":
+
+    class_obj = DivideTwo(9)
+    res = class_obj.divide_into_two()
+
+    print(res)
