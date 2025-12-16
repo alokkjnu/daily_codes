b8c9446b0864979da9d663d51124d78b7f0fed45
Alok <ry7069577@gmail.com>
2025-12-17 01:11:55 +0530

    updated code

--- class_based_oops/fastapi_reverse_string.py
+++ class_based_oops/fastapi_reverse_string.py
@@ -12,25 +12,24 @@ class Data(BaseModel):
 
     name : str
 
+
 app = FastAPI()
 
 @app.post("/")
 async def post_name(data:Data):
 
     try:
+
         name = data.name
-
-        res = ""
-
+        res= ""
         for i in name:
-
-            res = i + res
+            res= i+res
         
         return {"response":res}
+
     except Exception as e:
         return {"error":e}
 
+if __name__ == "__main__":
 
-if __name__=="__main__":
-
-    uvicorn.run(app,host="127.0.0.1",port=8002)
+    uvicorn.run(app,host="127.0.0.1",port=8003)
