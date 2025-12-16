0f5aa3603867a71ede0a08f48383c5881e87c911
Alok <ry7069577@gmail.com>
2025-12-17 00:43:44 +0530

    updated code

---
+++ class_based_oops/fastapi_reverse_string.py
@@ -0,0 +1,36 @@
+"""
+write a api using FASTAPI to return revese of string in response.
+input:"Alok"
+output:"kolA"
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+class Data(BaseModel):
+
+    name : str
+
+app = FastAPI()
+
+@app.post("/")
+async def post_name(data:Data):
+
+    try:
+        name = data.name
+
+        res = ""
+
+        for i in name:
+
+            res = i + res
+        
+        return {"response":res}
+    except Exception as e:
+        return {"error":e}
+
+
+if __name__=="__main__":
+
+    uvicorn.run(app,host="127.0.0.1",port=8002)
