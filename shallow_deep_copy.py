f341d398847ec94ee414a89f1019e815aa535a50
Alok Kumar Maurya <ry7069577@gmail.com>
2025-12-25 14:32:53 +0530

    updated code

--- shallow_deep_copy.py
+++ shallow_deep_copy.py
@@ -1,17 +1,21 @@
 import copy
-a = [1,2,34,4,5,6,23]
+
+a = [[1, 2], [3, 4]]
 b = copy.copy(a)
 
-b[0] = 10
+b[0][0] = 99
+
+
 print(a)
 print(b)
 
 
-
-a = [1,2,34,4,5,6,00]
+a = [[1, 2], [3, 4]]
 b = copy.deepcopy(a)
 
-b[0] = 55
+b[0][0] = 99
+
+
 print(a)
 print(b)
 
