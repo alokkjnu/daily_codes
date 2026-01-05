d7bbc617d9bad98a47aa47ce91dae25e1ae1e00d
Alok Kumar Maurya <ry7069577@gmail.com>
2026-01-05 13:34:08 +0530

    updated code

---
+++ class_based_oops/remove_duplicate_int.py
@@ -0,0 +1,35 @@
+"""write a program to remove duplicate from 
+list without using set or inbuit method
+
+input:[1,2,3,4,5,4,2,2,1,5,4,65,6,7,8,9,
+0,0,8,8,7,6,5,5,4,4,43,4,5,6,7,8,8,1,8,9]
+output: [1, 2, 3, 4, 5, 65, 6, 7, 8, 9, 0, 43]
+"""
+
+class RemoveDuplicateInteger:
+
+    def __init__(self,inp):
+
+        self.inp = inp
+
+    def remove_duplicates(self):
+
+        res = []
+
+        if len(self.inp) <= 1:
+            return self.inp
+        
+        for i in self.inp:
+            if i not in res:
+                res.append(i)
+
+        return res
+
+
+if __name__ == "__main__":
+
+    inp =[1,2,3,4,5,4,2,2,1,5,4,65,6,7,8,9,
+            0,0,8,8,7,6,5,5,4,4,43,4,5,6,7,8,8,1,8,9]
+    cls_obj = RemoveDuplicateInteger(inp)
+    res = cls_obj.remove_duplicates()
+    print(res)
