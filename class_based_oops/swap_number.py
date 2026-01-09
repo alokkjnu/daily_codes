abf06b0ebaa01c22f27d66b7e68dff1b907bc3e2
Alok Kumar Maurya <ry7069577@gmail.com>
2026-01-09 13:04:26 +0530

    updated code

---
+++ class_based_oops/swap_number.py
@@ -0,0 +1,31 @@
+"""
+Write a Python porgram to swap numbers
+without using third variabl.
+inp : a=5,b=4
+
+output: a=4, b=5
+"""
+
+class SwapNumbers:
+
+    def __init__(self,a,b):
+
+        self.a = a
+        self.b = b
+
+    def swap_numbers(self):
+
+        a = self.a + self.b
+        b = a - self.b
+        a = a - b
+
+        return a,b
+
+
+if __name__ == "__main__":
+
+    a = 5
+    b = 4
+    cls_obj = SwapNumbers(a,b)
+    res = cls_obj.swap_numbers()
+    print(res)
