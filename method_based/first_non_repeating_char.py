17c5f69a32de495582d79396763326cab5149911
Alok Kumar Maurya <ry7069577@gmail.com>
2026-01-23 12:00:00 +0530

    updated code

--- method_based/first_non_repeating_char.py
+++ method_based/first_non_repeating_char.py
@@ -9,6 +9,7 @@ Check that the current character does not
 appear in any previous or subsequent part of the string.
 
 Return the first character that occurs only once.
+
 inp1 = swiss
 inp2 = statistics
 inp3 = collector
