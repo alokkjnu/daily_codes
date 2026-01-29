0c40e5335d1be6fa642588894217d2b66928433b
Alok Kumar Maurya <ry7069577@gmail.com>
2026-01-29 12:00:00 +0530

    updated code

---
+++ class_based_oops/merge_sort.py
@@ -0,0 +1,46 @@
+"""
+Write a program to sort the list
+ element using Merge sort algorithm.
+input= [5,4,3,2,6,7,8,9,1]
+"""
+
+class MergeSort:
+
+    def merge_sort(self,inp):
+        if len(inp) <=1:
+            return inp
+        
+        mid = len(inp) //2
+        left = inp[:mid]
+        right = inp[mid:]
+
+        left = self.merge_sort(left)
+        right = self.merge_sort(right)
+
+        return self.merge(left,right)
+    @staticmethod
+    def merge(left,right):
+
+        sorted_list = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                sorted_list.append(left[i])
+                i+=1
+            else:
+                sorted_list.append(right[j])
+                j+=1
+        while i < len(left):
+            sorted_list.append(left[i])
+            i+=1
+        while j < len(right):
+            sorted_list.append(right[j])
+            j +=1
+
+        return sorted_list
+
+if __name__ == "__main__":
+    inp = [5,4,3,2,6,7,8,9,1]
+    cls_obj = MergeSort()
+    res = cls_obj.merge_sort(inp)
+    print(res)
