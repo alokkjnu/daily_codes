2e97fa391513b011f782b3325e9f3887e75bfe2d
Alok Kumar Maurya <ry7069577@gmail.com>
2026-01-30 12:00:00 +0530

    updated code

---
+++ class_based_oops/first_non_repeating_char.py
@@ -0,0 +1,17 @@
+"""
+Write a Python program to find and return 
+the first non-repeating character in a given string.
+The program should:
+
+Traverse the string character by character.
+
+Check that the current character does not
+appear in any previous or subsequent part of the string.
+
+Return the first character that occurs only once.
+inp1 = swiss
+inp2 = statistics
+inp3 = collector
+"""
+    
+
