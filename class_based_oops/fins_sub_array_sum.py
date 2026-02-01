96fa72d1c842a3e4e1e443207f1329b6c325768f
Alok Kumar Maurya <ry7069577@gmail.com>
2026-02-01 12:00:00 +0530

    updated code

--- class_based_oops/fins_sub_array_sum.py
+++ class_based_oops/fins_sub_array_sum.py
@@ -21,12 +21,13 @@ class FindSubArrSum:
             for j in range(i,self.inp_len):
                 sub_arr_sum += self.inp[j]
                 total_sum += sub_arr_sum
+                
         return total_sum
     
 if __name__ == "__main__":
 
     inp = [1,2,3,4,5,6,7,8]
-    
+
     cls_ob = FindSubArrSum(inp)
     res = cls_ob.find_sum()
 
